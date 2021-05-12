0cc3bfd8abf0e017ff647a37ce1e1d99cdc4b25d
ps_chap <chapmmch@gmail.com>
2021-05-11 20:20:41 -0400

    Edited LoadROM method and added private methods to Chip8Processor class that will be used to execute each of the 35 Chip-8 opcodes.

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,9 +1,40 @@
 #include "chip8.h"
 #include <cstdint>
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <chrono>
+#include <time.h>
 
 namespace CHIP8
 {
+	#pragma region Chip8Processor
+	Chip8Processor::Chip8Processor()
+	{
+		unsigned int i;
+
+		/* Initialize the program counter */
+		pc = START_ADDRESS;
+
+		/* Load fonts into memory */
+		for (i = 0; i < FONTSET_START_ADDRESS; i++)
+		{
+			memory[FONTSET_START_ADDRESS + i] = fontset[i];
+		}
+
+		opcode = 0;
+		index = 0;
+		sp = 0;
+
+		delay_timer = 0;
+		sound_timer = 0;
+
+		/* Initialize random seed. Cast to unsigned int to suppress warnings.  */
+		srand( (unsigned int)time(NULL));
+	}
+	#pragma endregion
+
+	#pragma region LoadROM
 	int Chip8Processor::LoadROM(const char *filename)
 	{
 		FILE *romFile;
@@ -11,11 +42,13 @@ namespace CHIP8
 		char *buffer;
 		size_t elements_read;
 		int error;
+		long i;
 
-		romFile = fopen(filename, "rb");
-
-		if (romFile != NULL)
+		try
 		{
+			if ((error = fopen_s(&romFile, filename, "rb")) != 0)
+				throw error;
+
 			// Get the size of the ROM file
 			fseek(romFile, 0, SEEK_END);
 			file_size = ftell(romFile);
@@ -25,16 +58,31 @@ namespace CHIP8
 			buffer = new char[file_size];
 			elements_read = fread(buffer, sizeof(uint8_t), file_size, romFile);
 			error = elements_read == file_size;
+			
+			if (!error)
+				throw error;
 
-			fclose(romFile);
-			delete[] buffer;
+			/* Copy buffer into main memory at the specified starting address */
+			for (i = 0; i < file_size; i++)
+			{
+				memory[START_ADDRESS + i] = buffer[i];
+			}
+
+			/* 
+			 * Initialize random seed so that we can generate random 8-bit unsigned integers.
+			 * Cast to unsigned int to suppress warnings.
+			*/
+			srand((unsigned int)time(NULL));
+
 		}
-		else
+		catch (int error)
 		{
-			fputs("Error reading CHIP-8 ROM File", stderr);
-			error = 0;
+			std::cerr << "Unable to read ROM file. Received error code: " << error << std::endl;
 		}
 
 		return error;
 	}
+	#pragma endregion
+
+
 }
--- src/chip8.h
+++ src/chip8.h
@@ -2,6 +2,7 @@
 #define _CHIP8_H_
 
 #include <cstdint>
+#include <random>
 
 namespace CHIP8
 {
@@ -16,9 +17,10 @@ namespace CHIP8
 			static const unsigned int DISPLAY_HEIGHT = 32;
 			static const unsigned int START_ADDRESS = 0X200;
 			static const unsigned int FONTSET_SIZE = 80;
+			static const unsigned int FONTSET_START_ADDRESS = 0x50;
 
 			/* Memory layout */
-			uint8_t V[NUM_REGISTERS]{ };
+			uint8_t V[NUM_REGISTERS] { };
 			uint8_t memory[MEMORY_LOCATIONS]{ };
 			uint16_t index;
 			uint16_t pc;
@@ -57,6 +59,46 @@ namespace CHIP8
 				0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
 				0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 			};
+
+			/*
+			 * Functions to execute each of the 35 Chip-8 opcodes. List of opcodes can be
+			 * found here: https://en.wikipedia.org/wiki/CHIP-8
+			 */
+			void opcode_0NNN();
+			void opcode_00E0();
+			void opcode_00EE();
+			void opcode_1NNN();
+			void opcode_2NNN();
+			void opcode_3XNN();
+			void opcode_4XNN();
+			void opcode_5XY0();
+			void opcode_6XNN();
+			void opcode_7XNN();
+			void opcode_8XY0();
+			void opcode_8XY1();
+			void opcode_8XY2();
+			void opcode_8XY3();
+			void opcode_8XY4();
+			void opcode_8XY5();
+			void opcode_8XY6();
+			void opcode_8XY7();
+			void opcode_8XYE();
+			void opcode_9XY0();
+			void opcode_ANNN();
+			void opcode_BNNN();
+			void opcode_CXNN();
+			void opcode_DXYN();
+			void opcode_EX9E();
+			void opcode_EXA1();
+			void opcode_FX07();
+			void opcode_FX0A();
+			void opcode_FX15();
+			void opcode_FX18();
+			void opcode_FX1E();
+			void opcode_FX29();
+			void opcode_FX33();
+			void opcode_FX55();
+			void opcode_FX65();
 
 		public:
 			/* Constructor initializes memory */
